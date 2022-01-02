83bc2da1ef671ff16073f73b42c1024bdc2bf7ad
Kaztro-Sir <94900172+Nihad-Kaztro@users.noreply.github.com>
2022-01-02 16:25:05 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -2,7 +2,7 @@ let fetch = require ('node-fetch')
 let handler = async (m, { conn }) => {
     let devil = `ㅤ
 `.trim()
-    await conn.send2ButtonLoc(m.chat, await (await fetch("https://github.com/Nihad-Kaztro/Medusa-Max/blob/master/Nihad-Kaztro.jpg")).buffer(), devil, 'ᴍᴇᴅᴜsᴀ-ᴍᴀx ʙʏ ɴɪʜᴀᴅ-ᴋᴀᴢᴛʀᴏ\n\╔╗╔╗╔══╗╔══╗\n\║╚╝║║╔╗║╚║║╝\n\║╔╗║║╠╣║╔║║╗\n\╚╝╚╝╚╝╚╝╚══╝ \n\n\ https://github.com/Nihad-Kaztro/Medusa-Max \n\n\ © ī.am ꪶᴍᴇᴅᴜsᴀ ᴍᴀxꫂ⁩⁴⁰⁴⁩' , '❤️', '.❤️', '🙂', ',🙂')
+    await conn.send2ButtonLoc(m.chat, await (await fetch("https://telegra.ph/file/7589694a1f03a3041bac4.jpg")).buffer(), devil, 'ᴍᴇᴅᴜsᴀ-ᴍᴀx ʙʏ ɴɪʜᴀᴅ-ᴋᴀᴢᴛʀᴏ\n\╔╗╔╗╔══╗╔══╗\n\║╚╝║║╔╗║╚║║╝\n\║╔╗║║╠╣║╔║║╗\n\╚╝╚╝╚╝╚╝╚══╝ \n\n\ https://github.com/Nihad-Kaztro/Medusa-Max \n\n\ © ī.am ꪶᴍᴇᴅᴜsᴀ ᴍᴀxꫂ⁩⁴⁰⁴⁩' , '❤️', '.❤️', '🙂', ',🙂')
 }
 handler.tags = ['main']
 handler.help = ['git']
